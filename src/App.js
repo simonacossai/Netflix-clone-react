77c0f60f6b18365f831cc9dfc50d48c3966b9f97
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2020-11-20 15:35:40 +0100

    adds modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
-import MyFooter from "./components/footer"
+import MyFooter from "./components/footer";
 import Navbar from './components/Navbar';
+import ModalForm from "./components/Modal"
+
 
 function App() {
   return (
    <>
    <Navbar />
+   <ModalForm/>
    <MovieList/>
    <MyFooter/>
    </>
---
+++ src/components/Modal.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+class ModalForm extends React.Component {
+  state = {
+    show: false,
+    movies: this.props.data ? this.props.data : {},
+  };
+
+  onChangeHandler = (e) => {
+    this.setState({
+      movies: {
+        ...this.state.movies,
+        [e.target.id]: e.target.value,
+      },
+    });
+  };
+
+  handleSubmit = async () => {
+    const url =
+      this.props.method === "PUT"
+        ? "https://striveschool-api.herokuapp.com/api/product/" +
+          this.props.data._id
+        : "https://striveschool-api.herokuapp.com/api/product/";
+    try {
+      const response = await fetch(url, {
+        method: this.props.method,
+        body: JSON.stringify(this.state.movies),
+        headers: new Headers({
+          "Content-Type": "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmI2NjhlNjk4MzViMDAwMTc1ODRlZTciLCJpYXQiOjE2MDU3ODk5MjYsImV4cCI6MTYwNjk5OTUyNn0.4C10FpKd6Z_zglQM7bO9F87KNlDxjd5t-7JQnKV6WR0",
+        }),
+      });
+      if (response.ok) {
+        alert("ok");
+        this.props.refetchData();
+        this.setState({ show: false });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <>
+        <Button variant="primary" onClick={() => this.setState({ show: true })}>
+          {this.props.btn}
+        </Button>
+
+        <Modal
+          show={this.state.show}
+          onHide={() => this.setState({ show: false })}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            sajdhksjdhjasjksahdhjsdhksahdsgdksfghkdsgfhdbfsfhbfdskdhfbdsb
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" >
+              Post data
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+}
+export default ModalForm;
